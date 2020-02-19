e30869853f03ea95421c220cdbab7d976ec089b2
Nate Harris <n8gr8gbln@gmail.com>
2020-02-18 23:05:01 -0500

    v1.0.1 release on PyPi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'bestbuy',         # How you named your package folder (MyLib)
   packages = ['bestbuy'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '1.0.1',      # Start with a small number and increase it with every change you make
   license='GNU GPLv3',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Use BestBuy APIs in Python',   # Give a short description about your library
   long_description_content_type='text/markdown',
@@ -11,7 +11,7 @@ setup(
   author = 'Nate Harris',                   # Type in your name
   author_email = 'n8gr8gbln@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/nwithan8/BestBuy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/nwithan8/BestBuy/archive/v1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/nwithan8/BestBuy/archive/v1.0.1.tar.gz',    # I explain this later on
   keywords = ['BestBuy', 'Best Buy', 'shopping', 'API', 'developer', 'products', 'technology', 'electronics'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
